ee4c30b56922a4ef45abbf73e820de1216004b45
Henry Xiao <xiaohenry08@gmail.com>
2015-10-03 12:07:36 -0700

    addingg keys

--- public/js/app.js
+++
@@ -1,9 +0,0 @@
-var HelloWorld = React.createClass({
-    render: function() {
-        return <div>Hello, world!</div>;
-    }
-});
-
-document.body.append(React.render(<HelloWorld />, document.body));
-document.body.append(React.render(<HelloWorld />, document.body));
-document.body.append(React.render(<HelloWorld />, document.body));
---
+++ public/js/popup.js
@@ -0,0 +1,1 @@
+Parse.initialize("Z3nuFrbx6y0SePEppNU6hx0tw3OrnkF7jPT6qgrD", "OKCMZeIYJjH4wf0UYmGZjdCOFqWykIIksNFmtpeg");
